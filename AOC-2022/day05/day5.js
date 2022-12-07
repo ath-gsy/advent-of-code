72075fcd88712535f3751e5e64546e11f79d6017
ath-gsy <107257470+ath-gsy@users.noreply.github.com>
2022-12-07 16:59:25 +0100

    day 5

--- AOC-2022/day05/day5.js
+++ AOC-2022/day05/day5.js
@@ -2,12 +2,11 @@ fs = require('fs');
 var data = fs.readFileSync('./input.txt', 'utf8');
 data = data.split('\n\n');
 
-// console.log(data);
-
+///CLASS
 class Pile {
   constructor(initialStack) {
     this.stack = initialStack;
-    this.height = initialStack.length;
+    this.height = this.stack.length;
   }
 
   isEmpty() {
@@ -33,16 +32,21 @@ class Pile {
   get() {
     return this.stack;
   }
-}
 
-console.log(data[0]);
+  set(table) {
+    this.stack = table;
+    this.height = table.length;
+  }
+}
+///
 
 initialConfig = data[0].split('\n');
 numberLines = initialConfig.length;
 
 initialConfig = initialConfig.map((line) => line.split(''));
-console.log(initialConfig);
 
+///renvoie le numéro du caractère qu'il faut chercher sur
+///chaque ligne pour créer les piles (on itère sur la dernière ligne)
 const findIndex = (pileNumber) => {
   for (var i = 0; i < initialConfig[numberLines - 1].length - 1; i++) {
     if (pileNumber === parseInt(initialConfig[numberLines - 1][i])) {
@@ -51,23 +55,56 @@ const findIndex = (pileNumber) => {
   }
 };
 
-// console.log(findIndex(1));
-
+///renvoie la pile au numéro désigné
 const initStack = (pileNumber) => {
   indexPile = findIndex(pileNumber);
   stack = [];
 
-  for (var i = numberLines - 1; i >= 0; i--) {
-    console.log(initialConfig[i][indexPile]);
-    if (initialConfig[i][indexPile] === '') {
-      return new Pile(stack);
+  for (var i = numberLines - 2; i >= 0; i--) {
+    if (initialConfig[i][indexPile] === ' ') {
+      return stack;
     } else {
       stack.push(initialConfig[i][indexPile]);
     }
   }
+  return stack;
 };
 
-pile1 = initStack(1);
-console.log(typeof pile1);
+///Création des piles
+cratesHolder = [];
+for (var i = 1; i < 10; i++) {
+  pile = new Pile([]);
+  pile.set(initStack(i));
+  cratesHolder.push(pile);
+}
 
-pile1.get();
+///instruction parser
+const instructionParsing = (instruction) => {
+  tab = instruction.split(' ');
+  return { quantity: tab[1], origin: tab[3], destination: tab[5] };
+};
+
+instructionText = data[1].split('\n');
+instructions = [];
+
+for (var i = 0; i < instructionText.length; i++) {
+  instructions.push(instructionParsing(instructionText[i]));
+}
+
+const crateMovement = (origin, destination) => {
+  crate = cratesHolder[origin - 1].stack.pop();
+  cratesHolder[destination - 1].stack.push(crate);
+};
+
+for (var i = 0; i < instructions.length; i++) {
+  for (var j = 0; j < instructions[i].quantity; j++) {
+    crateMovement(instructions[i].origin, instructions[i].destination);
+  }
+}
+
+s = '';
+for (var i = 0; i < cratesHolder.length; i++) {
+  s += cratesHolder[i].stack[cratesHolder[i].stack.length - 1];
+}
+
+console.log(s);
