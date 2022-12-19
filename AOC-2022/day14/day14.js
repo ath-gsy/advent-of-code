4c8dce2634c69031416a06641f5a050804b8e444
ath-gsy <107257470+ath-gsy@users.noreply.github.com>
2022-12-19 17:36:51 +0100

    day14 part2 - can't see why it doesn't work

--- AOC-2022/day14/day14.js
+++ AOC-2022/day14/day14.js
@@ -5,14 +5,14 @@ const rawData = fs.readFileSync('./input.txt', 'utf8');
 ///Initializing the cave plan
 var cavePlan = new Array(200);
 for (var i = 0; i < cavePlan.length; i++) {
-  cavePlan[i] = new Array(600);
+  cavePlan[i] = new Array(1000);
 }
 for (var i = 0; i < cavePlan.length; i++) {
   for (var j = 0; j < cavePlan[0].length; j++) {
     cavePlan[i][j] = '.';
   }
 }
-cavePlan[0][499] = '+';
+// cavePlan[0][499] = '+';
 ///
 ///Functions
 const cavePlanPrinter = (cavePlan) => {
@@ -68,7 +68,7 @@ cavePlanPrinter(cavePlan);
 const fallingSand = (startingPoint) => {
   for (var i = startingPoint.y; i < cavePlan.length - 1; i++) {
     if (i === cavePlan.length - 2) {
-      console.log('ERROR_no bound');
+      console.log('no bound');
       return [false];
     }
 
@@ -93,14 +93,63 @@ const fallingSand = (startingPoint) => {
 };
 ///
 /// COUNTING
-COUNTER = 0;
-inside = true;
-while (inside) {
-  COUNTER++;
-  inside = fallingSand({ x: 499, y: 1 })[0];
-  if (COUNTER % 10 == 0) {
-    cavePlanPrinter(cavePlan);
-  }
-}
-console.log(COUNTER - 1);
+// COUNTER = 0;
+// var inside = true;
+// while (inside) {
+//   COUNTER++;
+//   inside = fallingSand({ x: 499, y: 1 })[0];
+//   if (COUNTER % 10 == 0) {
+//     cavePlanPrinter(cavePlan);
+//   }
+// }
+// console.log(COUNTER - 1);
 ///
+
+//// PART 2 ////
+drawLine({ x: 0, y: 164 }, { x: cavePlan[0].length, y: 164 });
+cavePlanPrinter(cavePlan);
+
+const fallingSand2 = (startingPoint) => {
+  for (var i = startingPoint.y; i < cavePlan.length - 1; i++) {
+    if (cavePlan[0][499] === 'o') {
+      console.log('full');
+      return [false];
+    }
+
+    if (cavePlan[i + 1][startingPoint.x] === 'o' || cavePlan[i + 1][startingPoint.x] === '#') {
+      if (cavePlan[i + 1][startingPoint.x - 1] === 'o' || cavePlan[i + 1][startingPoint.x - 1] === '#') {
+        if (cavePlan[i + 1][startingPoint.x + 1] === 'o' || cavePlan[i + 1][startingPoint.x + 1] === '#') {
+          cavePlan[i][startingPoint.x] = 'o';
+
+          // cavePlanPrinter(cavePlan);
+
+          return [true, { x: startingPoint.x, y: i }];
+        } else {
+          // console.log('right free');
+          return fallingSand({ x: startingPoint.x + 1, y: i });
+        }
+      } else {
+        // console.log('left free');
+        return fallingSand({ x: startingPoint.x - 1, y: i });
+      }
+    }
+  }
+};
+
+COUNTER = 0;
+var notBlocked = true;
+while (notBlocked) {
+  COUNTER++;
+  notBlocked = fallingSand2({ x: 499, y: 0 })[0];
+  // if (cavePlan[0][499] === 'o') {
+  //   notBlocked = false;
+  // }
+}
+cavePlanPrinter(cavePlan);
+console.log(COUNTER - 1);
+
+// cavePlanPrinter(cavePlan);
+// for (var i = 0; i < 24727; i++) {
+//   fallingSand2({ x: 499, y: 0 });
+// }
+// cavePlanPrinter(cavePlan);
