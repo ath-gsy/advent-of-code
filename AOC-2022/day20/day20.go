2617f06a1fb542584b19d07b8a47e8d4e187245b
ath-gsy <107257470+ath-gsy@users.noreply.github.com>
2022-12-20 21:21:19 +0100

    day20 - practically finished (small adjustments needed)

--- AOC-2022/day20/day20.go
+++ AOC-2022/day20/day20.go
@@ -5,17 +5,131 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"strconv"
 	"strings"
 )
 
-//go:embed input.txt
-var input string
+type MixValue struct {
+	mixingOrder int
+	value       int
+}
 
-func main() {
+var (
+	//go:embed exemple.txt
+	input string
 
+	globalMap   = make(map[int]MixValue)
+	totalLength = 0
+)
+
+func init() {
 	input := strings.TrimSuffix(input, "\n")
 	lines := strings.Split(input, "\n")
 
-	fmt.Println(lines[0])
+	for originalOrder, value := range lines {
 
+		var value, _ = strconv.Atoi(value)
+		globalMap[originalOrder] = MixValue{originalOrder, value}
+	}
+
+	totalLength = len(lines)
+	// fmt.Println(totalLength)
+}
+
+func main() {
+	fmt.Println(globalMap)
+
+	originalOrder := 0
+	for originalOrder < totalLength {
+		mixOrder(originalOrder)
+		originalOrder++
+	}
+
+	fmt.Println(globalMap)
+
+}
+
+func mapkey(id int) (key int) { //, ok bool en sortie enlevé
+	for k, v := range globalMap {
+		if v.mixingOrder == id {
+			key = k
+			// ok = true
+			return
+		}
+	}
+	return
+}
+
+func mixOrder(originalOrder int) {
+	var displacementValue int = globalMap[originalOrder].value
+	var mixOrder = globalMap[originalOrder].mixingOrder
+
+	var newOrder = (mixOrder + displacementValue) % totalLength
+
+	newEntry := globalMap[originalOrder]
+	newEntry.mixingOrder = newOrder
+
+	var tempValue MixValue
+	var tempMapKey int
+
+	if displacementValue > 0 { //vers le bas ou...
+		if newOrder < mixOrder { //cas particulier overlap
+			for i := mixOrder; i < totalLength-1; i++ {
+				tempMapKey = mapkey(i + 1)
+				tempValue = globalMap[tempMapKey]
+				tempValue.mixingOrder = i
+				globalMap[tempMapKey] = tempValue
+			}
+			//cas particulier de length à 0
+			tempMapKey = mapkey(0)
+			tempValue = globalMap[tempMapKey]
+			tempValue.mixingOrder = totalLength - 1
+			globalMap[tempMapKey] = tempValue
+			//////////////////////////////
+			for i := 0; i < newOrder; i++ {
+				tempMapKey = mapkey(i + 1)
+				tempValue = globalMap[tempMapKey]
+				tempValue.mixingOrder = i
+				globalMap[tempMapKey] = tempValue
+			}
+		} else {
+			for i := mixOrder; i < newOrder; i++ {
+				tempMapKey = mapkey(i + 1)
+				tempValue = globalMap[tempMapKey]
+				tempValue.mixingOrder = i
+				globalMap[tempMapKey] = tempValue
+
+			}
+		}
+	} else if displacementValue < 0 { //...vers le haut
+		if newOrder > mixOrder { //cas particulier overlap
+			for i := mixOrder; i > 1; i-- {
+				tempMapKey = mapkey(i - 1)
+				tempValue = globalMap[tempMapKey]
+				tempValue.mixingOrder = i
+				globalMap[tempMapKey] = tempValue
+			}
+			//cas particulier de 0 à length
+			tempMapKey = mapkey(totalLength - 1)
+			tempValue = globalMap[tempMapKey]
+			tempValue.mixingOrder = 0
+			globalMap[tempMapKey] = tempValue
+			//////////////////////////////
+			for i := totalLength - 1; i > newOrder; i-- {
+				tempMapKey = mapkey(i - 1)
+				tempValue = globalMap[tempMapKey]
+				tempValue.mixingOrder = i
+				globalMap[tempMapKey] = tempValue
+			}
+		} else {
+			for i := mixOrder; i > newOrder; i-- {
+				tempMapKey = mapkey(i - 1)
+				tempValue = globalMap[tempMapKey]
+				tempValue.mixingOrder = i
+				globalMap[tempMapKey] = tempValue
+			}
+		}
+	}
+
+	globalMap[originalOrder] = newEntry
 }
